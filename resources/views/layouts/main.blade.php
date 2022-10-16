90822bd9f2f0460395cccbe6b2de9f8bf134ef09
TugasZulhamAbidin <tugaszulhamabidin@gmail.com>
2022-10-16 16:42:46 +0700

    first commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Laravel</title>
+</head>
+<body>
+
+    @yield('container')
+    
+
+    @vite('resources/js/app.js')
+</body>
+
+</html>
