d8862ec0b9672201e9fc7c49c2823716c293419c
TugasZulhamAbidin <tugaszulhamabidin@gmail.com>
2023-01-11 20:41:46 +0700

    dataexport

--- app/Exports/dataexport.php
+++ app/Exports/dataexport.php
@@ -3,15 +3,21 @@
 namespace App\Exports;
 
 use App\Models\data;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
-class dataexport implements FromCollection
+class dataexport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        return data::all();
+        return data::select('nama_lengkap', 'jenis_kelamin', 'alamat_domisili', 'pendidikan_terakhir', 'jurusan', 'hari')->get();
+    }
+
+    public function headings(): array
+    {
+        return ["NAMA LENGKAP", "JENIS KELAMIN", "ALAMAT DOMISILI", "PENDIDIKAN TERAKHIR", "JURUSAN", "TANGGAL PENGESAHAN"];
     }
 }
